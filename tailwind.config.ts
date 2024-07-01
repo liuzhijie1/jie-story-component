e50a899f5a5a391619ffcead88256c1cd35c05fd
liujie <572890972@qq.com>
2024-07-01 19:33:05 +0800

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+import { colors, white, black, transparent, currentColor } from './src/tokens/colors'
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx,html}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    colors: {
+      ...colors,
+      white,
+      black,
+      transparent,
+      currentColor,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config
+
