c619762f23559b8e0d2f245e210ab4020a236555
liujie <572890972@qq.com>
2024-07-31 12:15:55 +0800

    增加Table组件

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@
 import { Link } from './components/Link'
 import { Menu } from './components/Menu'
 import { Icon } from './components/icon'
+import { FstlnTable } from './components/Table'
 
 import './global.css'
 
-export { Link, Menu, Icon }
+export { Link, Menu, Icon, FstlnTable }
